c3603f978d18b75a8ca0ae9b7c3c9217f2f72cce
mitch <mitch>
2006-06-13 12:49:39 +0000

    support for different CHANs

--- php/4chan.php
+++ php/4chan.php
@@ -1,4 +1,5 @@
 <?
+// $Id: 4chan.php,v 1.3 2006-06-13 12:49:39 mitch Exp $
 // cookie handling
 //if (isset($comic) && isset($id) && isset($tag)) {
 //  setcookie("lastVisited[$tag]", $id, time()+( 3600 * 24 * 365 * 5));
@@ -18,6 +19,7 @@
     <table>
       <tr>
         <th>File</th>
+        <th>Chan</th>
         <th>Board</th>
         <th>Nick</th>
         <th>Channel</th>
@@ -43,8 +45,12 @@ if ($find) {
 	}
 	fclose($idx);
       }
+      if (!$entry['CHAN']) {
+	$entry['CHAN'] = '4chan';
+      }
       echo "      <tr>\n";
       echo "        <td><a href=\"/4chan/{$entry['FILE']}\">{$entry['FILE']}</a> (<a href=\"{$entry['URL']}\">orig</a>)</td>\n";
+      echo "        <td>{$entry['CHAN']}</td>\n";
       echo "        <td>/{$entry['BOARD']}/</td>\n";
       echo "        <td>{$entry['NICK']}</td>\n";
       echo "        <td>{$entry['CHANNEL']}</td>\n";
@@ -59,6 +65,6 @@ if ($find) {
 
     <hr>
     <address><a href="mailto:mitch@cgarbs.de">Christian Garbs [Master Mitch]</a></address>
-    <p><small>$Revision: 1.2 $<br>$Date: 2006-06-12 21:14:59 $</small></p>
+    <p><small>$Revision: 1.3 $<br>$Date: 2006-06-13 12:49:39 $</small></p>
   </body>
 </html>
