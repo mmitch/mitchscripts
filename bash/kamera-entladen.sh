456f418272705db30f02b26de5a6eb24ac5c0d44
mitch <mitch>
2007-02-04 10:14:13 +0000

    count pictures remove empty directories bail out on error

--- bash/kamera-entladen.sh
+++ bash/kamera-entladen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# $Id: kamera-entladen.sh,v 1.1 2007-01-01 17:27:15 mitch Exp $
+# $Id: kamera-entladen.sh,v 1.2 2007-02-04 10:14:13 mitch Exp $
+
+set -e
+
 USBPATH=/mnt/usb_part
 PICPATH=$USBPATH/dcim/100pentx
 SAVE=/mnt/bilder/Fotos/more/upload_$(date +%Y%m%d)
@@ -15,12 +18,19 @@ mkdir $SAVEPATH
 echo saving at $SAVEPATH
 mount $USBPATH
 
-mv $PICPATH/* $SAVEPATH
+PICCOUNT=$(find $PICPATH -type f | wc -l)
+if [ $PICCOUNT -ge 1 ] ; then
+    echo "$PICCOUNT pictures to copy"
+    mv $PICPATH/* $SAVEPATH
+else
+    echo "nothing to copy"
+    rmdir $SAVEPATH
+fi
 
 echo finished
 
 sync
 umount $USBPATH
-eject $USBPATH
+eject $USBPATH || true
 
 echo unmounted
