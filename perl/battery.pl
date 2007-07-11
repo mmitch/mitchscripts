56fd4298bbe7f97ce2069f298b585bf424c2d316
mitch <mitch>
2007-07-11 18:57:34 +0000

    add short display mode for dwm toolbar add CPU frequency support

--- perl/battery.pl
+++ perl/battery.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl -w
-# $Id: battery.pl,v 1.6 2005-10-30 00:52:24 mitch Exp $
+# $Id: battery.pl,v 1.7 2007-07-11 18:57:34 mitch Exp $
 #
 # Show laptop battery status
 #
 use strict;
+
+my $status = 0;
+if (defined $ARGV[0] and $ARGV[0] eq '-s') {
+    # dwm status bar, short display mode
+    $status = 1;
+}
 
 # battery location
 my $procbattery = '/proc/acpi/battery/BAT0/state';
@@ -11,6 +17,11 @@ my $procmax     = '/proc/acpi/battery/BAT0/info';
 
 # temperature information
 my $proctemp    = '/proc/acpi/thermal_zone/THM0/temperature';
+
+# cpu speed
+my $procminf    = '/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq';
+my $procmaxf    = '/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq';
+my $proccurf    = '/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq';
 
 # defaults
 my $state  = 'AC';
@@ -62,20 +73,54 @@ while (my $line = <PROCTEMP>) {
 close PROCTEMP
     or die "can't close `$proctemp': $1";
 
-# print data
+open CPUFREQ, '<', $procminf
+    or die "can't open `$procminf': $1";
+my $minfreq = <CPUFREQ>;
+chomp $minfreq;
+close CPUFREQ
+    or die "can't close `$procminf': $1";
+
+open CPUFREQ, '<', $procmaxf
+    or die "can't open `$procmaxf': $1";
+my $maxfreq = <CPUFREQ>;
+chomp $maxfreq;
+close CPUFREQ
+    or die "can't close `$procmaxf': $1";
+
+open CPUFREQ, '<', $proccurf
+    or die "can't open `$proccurf': $1";
+my $curfreq = <CPUFREQ>;
+chomp $curfreq;
+close CPUFREQ
+    or die "can't close `$proccurf': $1";
+
+# compute data
 my $percent = $remain / $max;
 my $p = sprintf "%.0f", $percent*10;
 my $battery = '#' x $p . '.' x (10 - $p );
-printf "%s", $state;
+my ($calc, $hours, $mins);
 if ($state eq 'DC') {
-    my $calc = $remain / $rate;
-    my $hours = int $calc;
-    my $mins = int (($calc - $hours) * 60);
-    printf "  %d:%02dh left", $hours, $mins;
+    $calc = $remain / $rate;
+    $hours = int $calc;
+    $mins = int (($calc - $hours) * 60);
 } elsif ($state eq 'AC' and $rate > 0) {
-    my $calc = ($max - $remain) / $rate;
-    my $hours = int $calc;
-    my $mins = int (($calc - $hours) * 60);
-    printf "  %d:%02dh left", $hours, $mins;
+    $calc = ($max - $remain) / $rate;
+    $hours = int $calc;
+    $mins = int (($calc - $hours) * 60);
 }
-printf " \n[%s]  %4.1f%% \n%4.1fW  %4.1fV  %4.1fWh  %2d°C\n", $battery, $percent*100, $rate/1000, $volt/1000, $remain/1000, $temp;
+
+# print data
+if ($status) {
+    printf "%d:%02dh%s [%s] %.1fW %d°C [%s]\n",
+    $hours,
+    $mins,
+    ($state eq 'AC') ? '+' : '-',
+    $battery,
+    $rate/1000,
+    $temp,
+    ($curfreq == $minfreq) ? '\\..' : ($curfreq == $maxfreq) ? '../' : '.|.';
+} else {
+    printf "%s  %d:%02dh left \n", $state, $hours, $mins;
+    printf "[%s]  %4.1f%% \n%4.1fW  %4.1fV  %4.1fWh  %2d°C\n", $battery, $percent*100, $rate/1000, $volt/1000, $remain/1000, $temp;
+    printf "cpu %s\n", ($curfreq == $minfreq) ? 'slow' : (($curfreq == $maxfreq) ? 'fast' : 'intermediate');
+}
