9fb087aae03c088c2ef8bee34b0de9ebaa09beec
Christian Garbs <mitch@cgarbs.de>
2007-11-02 21:12:55 +0100

    really use 1024bit for DH

--- bash/openssl_keypair.sh
+++ bash/openssl_keypair.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# $Id: openvpn_conf.sh,v 1.10 2007-05-13 10:24:01 mitch Exp $
 
 # 2007 (c) by Christian Garbs <mitch@cgarbs.de>
 
@@ -106,7 +105,7 @@ echo CA created
 
 echo creating DH...
 
-openssl dhparam -out dh1024.pem 128
+openssl dhparam -out dh1024.pem 1024
 chmod 600 dh1024.pem
 
 echo DH created
