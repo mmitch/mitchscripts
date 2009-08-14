f68389dd87f36cb7ec4290fb18c52b4624b6a91b
Christian Garbs <mitch@cgarbs.de>
2009-08-14 20:41:05 +0200

    notify.sh: use different timeouts for different message types

--- bash/notify.sh
+++ bash/notify.sh
@@ -17,9 +17,11 @@ DZEN_JUSTIFY=l
 
 DZEN_FG_ERR='#fff'
 DZEN_BG_ERR='#f00'
+DZEN_TIMEOUT_ERR=15
 
 DZEN_FG_OK='#000'
 DZEN_BG_OK='#0f0'
+DZEN_TIMEOUT_OK=10
 
 MSGFILE=~/.notify
 MSGWAIT=5
@@ -42,17 +44,20 @@ while sleep "$MSGWAIT"; do
 		OK:*)
 		    FG="$DZEN_FG_OK"
 		    BG="$DZEN_BG_OK"
+		    TIMEOUT="$DZEN_TIMEOUT_OK"
 		    ;;
 		ERR:*)
 		    FG="$DZEN_FG_ERR"
 		    BG="$DZEN_BG_ERR"
+		    TIMEOUT="$DZEN_TIMEOUT_ERR"
 		    ;;
 		*)
 		    FG="$DZEN_FG"
 		    BG="$DZEN_BG"
+		    TIMEOUT="$DZEN_TIMEOUT"
 		    ;;
 	    esac
-	    echo "$LINE" | "$DZEN_BIN" -fg "$FG" -bg "$BG" -fn "$DZEN_FONT" -p "$DZEN_TIMEOUT" -ta "$DZEN_JUSTIFY"
+	    echo "$LINE" | "$DZEN_BIN" -fg "$FG" -bg "$BG" -fn "$DZEN_FONT" -p "$TIMEOUT" -ta "$DZEN_JUSTIFY"
 	done < "$WORKFILE"
 	rm "$WORKFILE"
 
