d82a1d6a5bed10268534822416137d70f17b2fe9
Christian Garbs <mitch@cgarbs.de>
2014-08-07 19:52:55 +0200

    knit.sh: remove unneeded variable expansion in arithmetic expressions

--- bash/knit.sh
+++ bash/knit.sh
@@ -32,8 +32,8 @@ GRIDTEXTMOD=5
 
 
 # calculate scale factors
-SCALE_X=$(( $TOTAL_SCALE / $MASCHEN ))
-SCALE_Y=$(( $TOTAL_SCALE / $REIHEN ))
+SCALE_X=$(( TOTAL_SCALE / MASCHEN ))
+SCALE_Y=$(( TOTAL_SCALE / REIHEN ))
 
 # get current image size
 SIZE=$( identify -verbose "${IMAGE_IN}" | grep Geometry: | sed -e 's/^.*Geometry:\s*//' -e 's/\+.*$//' )
@@ -42,27 +42,27 @@ SIZE_Y=${SIZE##*x}
 echo "image is $SIZE_X x $SIZE_Y pixels"
 
 # calculate future image size (one extra pixel for final grid line)
-NEW_X=$(( $SIZE_X * $SCALE_X + 1))
-NEW_Y=$(( $SIZE_Y * $SCALE_Y + 1))
+NEW_X=$(( SIZE_X * SCALE_X + 1))
+NEW_Y=$(( SIZE_Y * SCALE_Y + 1))
 echo "image will be scaled to $NEW_X x $NEW_Y pixels"
 
 # generate vertical line commands
-for (( X=0 ; $X <= $NEW_X ; X=X+$SCALE_X )) ; do
+for (( X=0 ; X <= NEW_X ; X=X + SCALE_X )) ; do
     DRAWSTR="${DRAWSTR} line $X,0 $X,$NEW_Y"
 done
 
 # generate horizontal line commands
-for (( Y=0 ; $Y <= $NEW_Y ; Y=Y+$SCALE_Y )) ; do
+for (( Y=0 ; Y <= NEW_Y ; Y=Y + SCALE_Y )) ; do
     DRAWSTR="${DRAWSTR} line 0,$Y $NEW_X,$Y"
 done
 
 # generate horizontal text commands
 MASCHE=0
-for (( X=0 ; $X <= $NEW_X ; X=X+$SCALE_X )) ; do
-    NR=$(( $SIZE_X - $MASCHE ))
-    if [ $(( $NR % $GRIDTEXTMOD )) = 0 ] ; then
-	TEXT="${TEXT} text $(($X + 2)),$(($NEW_Y - 2)) '$NR'"
-	TEXT="${TEXT} text $(($X + 2)),$(($SCALE_Y - 2)) '$NR'"
+for (( X=0 ; X <= NEW_X ; X=X + SCALE_X )) ; do
+    NR=$(( SIZE_X - MASCHE ))
+    if [ $(( NR % GRIDTEXTMOD )) = 0 ] ; then
+	TEXT="${TEXT} text $((X + 2)),$((NEW_Y - 2)) '$NR'"
+	TEXT="${TEXT} text $((X + 2)),$((SCALE_Y - 2)) '$NR'"
 	DRAWSTR="${DRAWSTR} line $((X + 1)),0 $((X + 1)),$NEW_Y"
     fi
     ((MASCHE++))
@@ -70,13 +70,13 @@ done
 
 # generate vertical text commands
 REIHE=-1
-for (( Y=0 ; $Y <= $NEW_Y ; Y=Y+$SCALE_Y )) ; do
-    NR=$(( $SIZE_Y - $REIHE ))
-    if [ $(( $NR % $GRIDTEXTMOD )) = 0 ] ; then
-	TEXT="${TEXT} text 2,$(($Y - 2)) '$NR'"
-	TEXT="${TEXT} text $(($NEW_X - $SCALE_X + 1)),$(($Y - 2)) '$NR'"
+for (( Y=0 ; Y <= NEW_Y ; Y=Y + SCALE_Y )) ; do
+    NR=$(( SIZE_Y - REIHE ))
+    if [ $(( NR % GRIDTEXTMOD )) = 0 ] ; then
+	TEXT="${TEXT} text 2,$((Y - 2)) '$NR'"
+	TEXT="${TEXT} text $((NEW_X - SCALE_X + 1)),$((Y - 2)) '$NR'"
     fi
-    if [ $(( $NR % $GRIDTEXTMOD )) = 1 ] ; then
+    if [ $(( NR % GRIDTEXTMOD )) = 1 ] ; then
 	DRAWSTR="${DRAWSTR} line 0,$((Y + 1)) $NEW_X,$((Y + 1))"
     fi
     ((REIHE++))
