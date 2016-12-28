af06b660d43059e71e69e9e86c67ac7d7bc38902
Christian Garbs <mitch@cgarbs.de>
2016-12-28 10:03:32 +0100

    don't notify about inventory reports

--- bash/mailnotify.sh
+++ bash/mailnotify.sh
@@ -3,7 +3,7 @@
 # simple X notification framework
 # process procmail mail logs and convert them to notify entries
 #
-# Copyright (C) 2009-2010,2014  Christian Garbs <mitch@cgarbs.de>
+# Copyright (C) 2009-2010,2014,2016  Christian Garbs <mitch@cgarbs.de>
 # Licensed under GNU GPL v3 or later.
 #
 
@@ -21,7 +21,7 @@ tail -n 1 -f ~/Mail/from | ~/git/mitchscripts/perl/mimedecoder.pl | while read L
 	FOLDER="${FOLDER%/new/*}"
 	FOLDER="${FOLDER#/home/mitch/Mail/}"
 
-	if [[ ! "${FOLDER}" =~ (spam|null|warpzone-ml) ]] ; then
+	if [[ ! "${FOLDER}" =~ (spam|null|warpzone-ml|receive-report.sh) ]] ; then
 	    echo "%%   $FOLDER   %%   $FROM   %%   $SUBJECT" >> ~/.notify
 	fi
     fi
