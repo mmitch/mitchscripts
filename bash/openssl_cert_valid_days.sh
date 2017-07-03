6bef44663f848f75d358b94ae17d82d82315244c
Christian Garbs <mitch@cgarbs.de>
2017-07-03 21:25:26 +0200

    allow multiple files

--- bash/openssl_cert_valid_days.sh
+++ bash/openssl_cert_valid_days.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #
 # show valid days left on SSL certificate
-
-# one-liner for multiple files:
-# NOW=$(date +%s); for CERT in *.pem; do ENDDATE=$(date --date "$(openssl x509 -enddate -noout -in $CERT | cut -f 2 -d =)" +%s); printf "%6d days - %s\n" $(( ( $ENDDATE - $NOW ) / ( 60 * 60 * 24 ) )) "$CERT"; done | sort -n
-
-# readable script for single file ($1):
-CERT="$1"
+#
+# usage:
+#   openssl_cert_valid_days.sh cert-pem [ cert-pem [...] ]
 
 NOW=$(date +%s)
-END=$(date --date "$(openssl x509 -enddate -noout -in "$CERT" | cut -f 2 -d =)" +%s)
 
-SECONDS_LEFT=$(( $END - $NOW ))
-DAYS_LEFT=$(( $SECONDS_LEFT / ( 60 * 60 * 24 ) ))
-
-printf "%6d days - %s\n" $DAYS_LEFT "$CERT"
+for CERT in "$@"; do
+    
+    END=$(date --date "$(openssl x509 -enddate -noout -in "$CERT" | cut -f 2 -d =)" +%s)
+    
+    SECONDS_LEFT=$(( $END - $NOW ))
+    DAYS_LEFT=$(( $SECONDS_LEFT / ( 60 * 60 * 24 ) ))
+    
+    printf "%6d days - %s\n" $DAYS_LEFT "$CERT"
+    
+done
