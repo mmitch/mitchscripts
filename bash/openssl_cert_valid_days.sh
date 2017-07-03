f1a2426169db97ed2e4d90c66b928bf68af5f594
Christian Garbs <mitch@cgarbs.de>
2017-07-03 21:31:55 +0200

    add maximum days filter
    don't warn about certificates with a longer validity

--- bash/openssl_cert_valid_days.sh
+++ bash/openssl_cert_valid_days.sh
@@ -3,9 +3,18 @@
 # show valid days left on SSL certificate
 #
 # usage:
-#   openssl_cert_valid_days.sh cert-pem [ cert-pem [...] ]
+#   openssl_cert_valid_days.sh [ -max days ] cert-pem [ cert-pem [...] ]
+
+set -e
 
 NOW=$(date +%s)
+
+MAX_DAYS=0
+
+if [ "$1" eq '-max' ]; then
+    MAX_DAYS=$2
+    shift 2
+fi
 
 for CERT in "$@"; do
     
@@ -13,6 +22,10 @@ for CERT in "$@"; do
     
     SECONDS_LEFT=$(( $END - $NOW ))
     DAYS_LEFT=$(( $SECONDS_LEFT / ( 60 * 60 * 24 ) ))
+
+    if [ $MAX_DAYS -gt 0 -a $DAYS_LEFT -gt $MAX_DAYS ]; then
+	next
+    fi
     
     printf "%6d days - %s\n" $DAYS_LEFT "$CERT"
     
