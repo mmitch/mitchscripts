0a1b84aefd2ff9e5a2e51fab315cdf7d2466704a
Christian Garbs <mitch@cgarbs.de>
2021-08-03 20:38:50 +0200

    generate square image; fill empty fields with black

--- bash/gimp_palette_to_svg.sh
+++ bash/gimp_palette_to_svg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # convert GIMP palette file to SVG image
 #
@@ -16,14 +16,49 @@ while read -r line; do
     [ "$line" = '#' ] && break
 done
 
-x=0
-y=0
+# read color lines into memory
+mapfile palette
+palette_length=${#palette[@]}
+
+# calculate smalles square to fit palette entries
+square_size=1
+squared_size=1
+while [ $squared_size -lt "$palette_length" ]; do
+    square_size=$(( square_size + 1 ))
+    squared_size=$(( square_size * square_size ))
+done
+
+# expand palette to full square
+while [ "$palette_length" -lt $squared_size ]; do
+    palette+=( "0 0 0 empty" )
+    palette_length=$(( palette_length + 1 ))
+done
+
+# print SVG header
 cat <<EOF
 <?xml version="1.0"?>
 <svg xmlns="http://www.w3.org/2000/svg">
 EOF
-while read -r r g b _; do
+
+i=0
+x=0
+y=0
+max_x=$(( square_size * size ))
+while [ $i -lt $palette_length ]; do
+    # parse palette line
+    read -r r g b _ <<< "${palette[$i]}"
+
+    # print palette box
     printf '  <rect x="%d" y="%d" width="%d" height="%d" style="fill:rgb(%d,%d,%d);" />\n' $x $y $size $size "$r" "$g" "$b"
+
+    # calculate next entry
+    i=$(( i + 1 ))
     x=$(( x + size ))
+    if [ $x -eq $max_x ]; then
+	x=0
+	y=$(( y + size ))
+    fi
 done
+
+# print SVG footer
 echo '</svg>'
