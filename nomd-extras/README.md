dd5d927fc4ee55409ae5a3dfea3cc78a1e501979
Christian Garbs <mitch@cgarbs.de>
2024-09-26 09:25:50 +0200

    add nomd-extras with logritate config checker

---
+++ nomd-extras/README.md
@@ -0,0 +1,6 @@
+nomd-extras
+===========
+
+This directory contains extra monitoring scripts for [nomd](https://github.com/mmitch/nomd)
+that I want to use on multiple systems (sharing them via this repository)
+but that aren't generic enough to include them in nomd itself.
---
+++ nomd-extras/check-logrotate-config.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+OK() {
+    echo "I:logrotate_config:$*"
+}
+
+FAIL() {
+    echo "I:logrotate_config:$*"
+}
+
+config_is_set()
+{
+    local what=$1 where=$2
+    grep -q -P "^\\s*$what\b" "$where"
+}
+
+config_must_be_set()
+{
+    local what=$1 where=$2
+    
+    if config_is_set "$what" "$where"; then
+	OK "option '$what' is set in $where"
+    else
+	FAIL "option '$what' is not set in $where"
+    fi
+}
+
+config_must_not_be_set()
+{
+    local what=$1 where=$2
+
+    if ! config_is_set "$what" "$where"; then
+	OK "option '$what' is not set in $where"
+    else
+	FAIL "option '$what' is set in $where"
+    fi
+}
+
+# dateext should always be enabled
+config_must_be_set dateext /etc/logrotate.conf
+
+# compress should be disabled only when we use ZFS
+if df -T /var/log | grep -q -F " zfs "; then
+
+    {
+	echo /etc/logrotate.conf
+	find /etc/logrotate.d -type f
+    } | while read -r config; do
+	config_must_not_be_set compress "$config"
+    done
+fi
